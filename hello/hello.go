3ed813c656a78b1fad113f40a9e283a0e27c8311
Jason Andrada <jpa323@nyu.edu>
2019-07-26 12:02:26 -0400

    add comments for clarity

--- hello/hello.go
+++ hello/hello.go
@@ -6,6 +6,10 @@ import (
 	"github.com/jase-d-ace/stringutil"
 )
 
+//this is the main brain of a golang program.
+//since the package is called "main," this file will be generate an executable that you use in your terminal
+//the executable then runs the main function of this file. In this case, it runs two simple functions that have been imported from other folders in this workspace.
+
 func main() {
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 	httpreqs.MakeReq()
