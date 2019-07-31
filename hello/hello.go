81840b118c8d6a980fd2ea5bc9bb6ceeed549885
Jason Andrada <jpa323@nyu.edu>
2019-07-31 08:44:30 -0400

    add practice with iterating over arrays of structs

--- hello/hello.go
+++ hello/hello.go
@@ -10,7 +10,23 @@ import (
 //since the package is called "main," this file will be generate an executable that you use in your terminal
 //the executable then runs the main function of this file. In this case, it runs two simple functions that have been imported from other folders in this workspace.
 
+type Example struct {
+	Foo, Bar, Boo string
+}
+
+var exArray = []Example{
+	Example{"something", "else", "yeah"},
+	Example{"no", "yes", "maybe"},
+}
+
+func printArray(arr []Example) {
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
+}
+
 func main() {
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 	httpreqs.MakeReq()
+	printArray(exArray)
 }
