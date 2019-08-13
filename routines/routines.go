70b90b99540694666d975811d758cc89313d9143
Jason Andrada <jpa323@nyu.edu>
2019-08-13 11:59:40 -0400

    add way to stop execution of main function

--- routines/routines.go
+++ routines/routines.go
@@ -6,16 +6,20 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func hello() {
 	fmt.Println("hello from goroutines")
 }
 
-//trying to execute the goroutine immediately like this will not work.
 //the goroutine returns immediately, so after executing, keyword "go" does not wait for the hello() function to execute
 
 func main() {
 	go hello()
+	//stopping the execution of the main function allows goroutines to work.
+	//there are many ways to stop the execution of the main function, and time.Sleep is one that illustrates the concept.
+	//tutorial says using channels is the prod way of stopping main from running while goroutines finish up
+	time.Sleep(1 * time.Second)
 	fmt.Println("hello from main")
 }
